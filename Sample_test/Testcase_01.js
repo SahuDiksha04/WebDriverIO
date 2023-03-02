44bb8a0be50e4bd4ac87069a5edaa3bcc03b3022
SahuDiksha04 <diksha41195@gmail.com>
2023-03-02 17:06:07 +0530

    updated

--- Sample_test/Testcase_01.js
+++ Sample_test/Testcase_01.js
@@ -6,44 +6,43 @@
 
 
 describe('My Login application', async() => {
-    it('should login with valid credentials', async () => {
+   let rm=Math.floor(Math.random()*(99-10)+10)
+   let userName="ajax1"+rm
+   let email="ajax1@gmail.com"+rm
+   let phoneNo="8676540558"+rm
+   let password="abc123"
+    
+   it('should login with valid credentials', async () => {
     await browser.maximizeWindow()
     await browser.url("http://testingserver/domain/Online_Food_Ordering_System/index.php");
 
    await browser.$('//a[text()="Register"]').click();
-   await browser.$('//input[@name="username"]').setValue("ajax1");
+   await expect(browser).toHaveTitleContaining("Registration");
+   await browser.$('//input[@name="username"]').setValue(userName);
    await browser.$('//input[@name="firstname"]').setValue("ajax1");
    await browser.$('//input[@name="lastname"]').setValue("lpu");
    
 
-   await browser.$('//input[@name="email"]').setValue("ajax1@gmail.com")
-   await browser.$('//input[@name="phone"]').setValue("8676540558");
-   await browser.$('//input[@name="password"]').setValue("2245678");
-   await browser.$('//input[@name="cpassword"]').setValue("2245678")
+   await browser.$('//input[@name="email"]').setValue(email)
+   await browser.$('//input[@name="phone"]').setValue(phoneNo);
+   await browser.$('//input[@name="password"]').setValue(password);
+   await browser.$('//input[@name="cpassword"]').setValue(password)
    await browser.$('//textarea[@id="exampleTextarea"]').setValue("btm stage 2 bangalore");
    await browser.$('//input[@name="submit"]').click();
-   await browser.pause('2000');
+   await expect(browser).toHaveTitleContaining("Login");
+   await browser.$('//input[@name="username"]').waitForDisplayed()
     })
-
-  
-   //It is to freeze the page
-   //await browser.debug()
-
-   //login
+   
    it('login to the page', async()=>{
-   await  browser.$('//input[@name="username"]').setValue("ajax1");
-   await  browser.$('//input[@name="password"]').setValue("2245678");
+   await  browser.$('//input[@name="username"]').setValue(userName);
+   await  browser.$('//input[@name="password"]').setValue(password);
    await  browser.$('//input[@name="submit"]').click();
-   //await browser.debug();
-    let data = await browser.getTitle()
-   //console.log(data);
-      await expect(browser).toHaveTitleContaining("Login")
+   await expect(browser).toHaveTitleContaining("Home");
    })
 
-   //logout
+   
    it('logout from the application', async()=>{
    await browser.$('//a[text()="Logout"]').click();
-   await browser.pause('5000');
    })
 
 })
